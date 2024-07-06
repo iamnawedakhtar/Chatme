37559673a76dd02c4d8cc41587505d9cc55958db
NAWED AKHTAR <nawedakhtar1811@gmail.com>
2024-07-06 14:21:57 +0530

    initial commit

---
+++ chat-with-friends/src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYdbDZb6Mkt_hpdX0s1CF74V1mbOhHIl0",
+    authDomain: "my-chat-app-5ca7a.firebaseapp.com",
+    projectId: "my-chat-app-5ca7a",
+    storageBucket: "my-chat-app-5ca7a.appspot.com",
+    messagingSenderId: "396635605440",
+    appId: "1:396635605440:web:224f864ba6027553c1a025"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
